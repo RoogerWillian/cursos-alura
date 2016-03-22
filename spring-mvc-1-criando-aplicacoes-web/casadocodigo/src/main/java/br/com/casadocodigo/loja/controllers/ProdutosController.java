6333434cdda457402b20bac64e63d5170cd97050
Roger Willian <roger.nizoli@gmail.com>
2016-03-22 11:56:05 -0300

    Cadastro de produtos

--- spring-mvc-1-criando-aplicacoes-web/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ spring-mvc-1-criando-aplicacoes-web/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -1,12 +1,19 @@
 package br.com.casadocodigo.loja.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import br.com.casadocodigo.loja.dao.ProdutoDAO;
+import br.com.casadocodigo.loja.model.Produto;
+
 @Controller
 @RequestMapping("/produtos")
 public class ProdutosController {
+	
+	@Autowired
+	private ProdutoDAO produtoDao;
 	
 	@RequestMapping("novo")
 	public String form(){
@@ -14,10 +21,8 @@ public class ProdutosController {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public String gravar(String titulo, String descricao, int paginas){
-		System.out.println(titulo);
-		System.out.println(descricao);
-		System.out.println(paginas);
+	public String gravar(Produto produto){
+		produtoDao.gravar(produto);
 		return "produtos/ok";
 	}
 }
