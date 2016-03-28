2cf8d629a6ef726596ea9712f87e54d8b2fc1c68
Roger Willian <roger.nizoli@gmail.com>
2016-03-28 13:09:26 -0300

    Preços

--- spring-mvc-1-criando-aplicacoes-web/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ spring-mvc-1-criando-aplicacoes-web/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.casadocodigo.loja.dao.ProdutoDAO;
 import br.com.casadocodigo.loja.model.Produto;
+import br.com.casadocodigo.loja.model.TipoPreco;
 
 @Controller
 @RequestMapping("/produtos")
@@ -16,8 +18,10 @@ public class ProdutosController {
 	private ProdutoDAO produtoDao;
 	
 	@RequestMapping("novo")
-	public String form(){
-		return "/produtos/form";
+	public ModelAndView form(){
+		ModelAndView mv = new ModelAndView("/produtos/form");
+		mv.addObject("tipos", TipoPreco.values());
+		return mv;
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
--- spring-mvc-1-criando-aplicacoes-web/casadocodigo/src/main/java/br/com/casadocodigo/loja/model/Produto.java
+++ spring-mvc-1-criando-aplicacoes-web/casadocodigo/src/main/java/br/com/casadocodigo/loja/model/Produto.java
@@ -1,5 +1,8 @@
 package br.com.casadocodigo.loja.model;
 
+import java.util.List;
+
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -17,6 +20,10 @@ public class Produto {
 	private String titulo;
 	private String descricao;
 	private int paginas;
+	
+	@ElementCollection
+	private List<Preco> precos;
+	
 	public String getTitulo() {
 		return titulo;
 	}
@@ -43,5 +50,21 @@ public class Produto {
 	@Override
 	public String toString() {
 		return "Produto [titulo=" + titulo + ", descricao=" + descricao + ", paginas=" + paginas + "]";
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public List<Preco> getPrecos() {
+		return precos;
+	}
+
+	public void setPrecos(List<Preco> precos) {
+		this.precos = precos;
 	}
 }
