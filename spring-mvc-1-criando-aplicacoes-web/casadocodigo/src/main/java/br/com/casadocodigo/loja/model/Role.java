b8e4d7249be8f5330ae9607db92c683a6226f18f
Roger Willian <roger.nizoli@gmail.com>
2016-04-10 20:59:58 -0300

    Spring Security

---
+++ spring-mvc-1-criando-aplicacoes-web/casadocodigo/src/main/java/br/com/casadocodigo/loja/model/Role.java
@@ -0,0 +1,27 @@
+package br.com.casadocodigo.loja.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.springframework.security.core.GrantedAuthority;
+
+@SuppressWarnings("serial")
+@Entity
+public class Role implements GrantedAuthority{
+
+	@Id
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String getAuthority() {
+		return this.nome;
+	}
+}
