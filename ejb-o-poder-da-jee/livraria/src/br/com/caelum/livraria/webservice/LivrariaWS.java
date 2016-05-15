95b30dab33b1bcec19e5bf4ead380b88e4619608
= <roger.nizoli@gmail.com>
2016-05-15 02:50:53 -0300

    livraria full

--- ejb-o-poder-da-jee/livraria/src/br/com/caelum/livraria/webservice/LivrariaWS.java
+++ ejb-o-poder-da-jee/livraria/src/br/com/caelum/livraria/webservice/LivrariaWS.java
@@ -2,6 +2,7 @@ package br.com.caelum.livraria.webservice;
 
 import java.util.List;
 
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
@@ -11,6 +12,7 @@ import br.com.caelum.livraria.dao.LivroDao;
 import br.com.caelum.livraria.modelo.Livro;
 
 @WebService
+@Stateless
 public class LivrariaWS {
 
 	@Inject
